95f95d169b42311cc530574bc44a4f865a3a1800
Ming Chow <mchow@cs.tufts.edu>
2019-06-24 02:33:58 +0000

    Updated SimpleExpress example, removed comments

--- examples/nodejs/simpleexpress/app.js
+++ examples/nodejs/simpleexpress/app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-// IP: 130.64.188.69
-
 /* HOW TO READ ROUTES USING EXPRESS FRAMEWORK:
 app.HTTP_VERB('ROUTE NAME', function(request, response) {
   // Do stuff here...
@@ -15,10 +13,6 @@ app.get('/', function(request, response) {
 
 app.get('/pikachu', function(request, response) {
 	response.send("You've won the game!");
-});
-
-app.get('/*', function(request, response) {
-	response.send("Go away! Run away!");
 });
 
 // Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
